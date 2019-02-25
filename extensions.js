1ade4fb344be41ad5236afb873a230009ac4961f
ポール ウェッブ <netopwibby@thenetwork.email>
2019-02-25 17:03:36 -0600

    Initial commit

---
+++ extensions.js
@@ -0,0 +1,39 @@
+"use strict";
+
+
+
+//  E X P O R T S
+
+module.exports = exports = [
+  "3gp",
+  "aac",
+  "act",
+  "aiff",
+  "amr",
+  "ast",
+  "au",
+  "bwf",
+  "dct",
+  "dss",
+  "flac",
+  "gsm",
+  "m4a",
+  "m4p",
+  "mmf",
+  "mp2",
+  "mp3",
+  "mp4",
+  "mpc",
+  "ogg",
+  "oga",
+  "opus",
+  "pac",
+  "ra",
+  "raw",
+  "s3m",
+  "sln",
+  "tta",
+  "vox",
+  "wav",
+  "wv"
+];
