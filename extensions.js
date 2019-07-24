03b324cd4ed73de048dad51272a6a00f79aa8205
netop://ウエハ <paul@webb.page>
2019-07-24 11:27:39 -0500

    Added comments

--- extensions.js
+++ extensions.js
@@ -5,24 +5,25 @@
 //  E X P O R T S
 
 module.exports = exports = [
+  // #
   "3gp",
-
+  // A
   "aac",
   "act",
   "aiff",
   "amr",
   "ast",
   "au",
-
+  // B
   "bwf",
-
+  // C
   "dct",
   "dss",
-
+  // F
   "flac",
-
+  // G
   "gsm",
-
+  // M
   "m4a",
   "m4p",
   "mmf",
@@ -30,23 +31,23 @@ module.exports = exports = [
   "mp3",
   "mp4",
   "mpc",
-
+  // O
   "ogg",
   "oga",
   "opus",
-
+  // P
   "pac",
-
+  // R
   "ra",
   "raw",
-
+  // S
   "s3m",
   "sln",
-
+  // T
   "tta",
-
+  // V
   "vox",
-
+  // W
   "wav",
   "wv"
 ];
